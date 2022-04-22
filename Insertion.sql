91806dc79012253f5d8f2999f230ef4eb03a8a40
yarlagaddajeevika <47218938+yarlagaddajeevika@users.noreply.github.com>
2022-04-22 13:52:10 -0700

    Add files via upload

---
+++ Insertion.sql
@@ -0,0 +1,10 @@
+use JOESCHOPS
+
+INSERT into Customer values ('Jeevika','Yarlagadda',999999999,'abc@gmail.com','jeevika','abcd','600 langsdorf','Fullerton','CA',92831)
+
+INSERT into Employee values ('Chao123','xyz','Wu','Chao','Manager',8888888888,'chao@gmail.com')
+
+INSERT into Vehicle values (80000000,'Sonic','1234A',1996,'LPW Naturally Aspirated Engines 2, 3 & 4 Cylinder Configurations ',
+							'SEL','Car Wash','Seat Adjustment','Average','New', 'Average', 'Average')
+
+insert into Customization_Detail values(80000000,1000,2)
--- db.py
+++ db.py
@@ -16,7 +16,8 @@ conn= connector.connect(connection_string)
 #create a cursor to work on the database
 cur=conn.cursor()
 
-### To authenticate user credentials
+##Login
+# To authenticate user credentials - read from employee, customer data
 def authenticateUser(username,password):
       if username=="" or password=="":
          return False
@@ -26,6 +27,74 @@ def authenticateUser(username,password):
          for i in cur:
             customer.append(i)
             if not customer:
-               return False
+               cur.execute(f"""SELECT Employee.Emp_ID FROM Employee WHERE username='{username}' AND upassword='{password}'""")
+               employee = []
+               for k in cur:
+                  employee.append(i)
+                  if not employee:
+                     return False
+                  else:
+                     return employee[0]
             else:
-               return customer[0]
+               return customer[0] # customer ID
+
+# Insert entry into Customer table
+def createCustomer(fName, lName, mobile, email,uname, upswd, address, city, state, zip):
+   if uname=="" or upswd=="" or fName=="" or lName=="" or mobile=="" or email=="" or address=="" or city=="" or zip=="":
+         return False
+      else:
+         cur.execute(f"""INSERT into Customer values ({fName},{lName},{mobile},{email},{uname},{upswd},{address},{city},{state},{zip})""")
+         print("Successfully inserted row into customer table")
+         return True
+
+##Customer
+#Insert data into vehicle table
+def StoreVehicleInfo(custId, producedBy, ModelNo, year, engineDet, Tirm, exterior, interior, bodyC, frameC, interC, engC):
+   if custId=="" or year=="":
+         return False
+      else:
+         cur.execute(f"""INSERT into Vehicle values ({custId},{producedBy},{ModelNo},{year},{engineDet},{Tirm},{exterior},{interior},{bodyC},{frameC},{interC},{engC})""")
+         print("Successfully inserted data into vehicle table")
+         return True
+
+#Dropdown values to show available employees
+def lisOfAllEmployees():
+   cur.execute(f"""select Employee.Emp_firstName+Employee.Emp_lastName from Employee""")
+   employeeData=[]
+   for i in cur:
+      employeeData.append(i)
+   
+   return employeeData # array with all employee names
+
+#Store details about Customisation
+def StoreCustomisationDetails(custId, empName, itemName, itemdesp):
+   if custId==""
+      return False
+   else:
+      cur.execute(f"""select Employee.Emp_ID from Employee where Employee.Emp_firstName+Employee.Emp_lastName={empName}""")
+      empID=cur[0]
+
+      cur.execute(f"""select Vehicle.VerificationIN from Vehicle where Vehicle.Cust_ID={custId}""")
+      vIn=cur[0]
+
+      #store data in Customization_Detail
+      cur.execute(f"""insert into Customization_Detail values({custId},{vIn},{empID})""")
+
+      cur.execute(f"""select Plan_ID from Customization_Detail where Cust_ID={custId}""")
+      planId = cur[0]
+
+      cur.execute(f"""insert into Item values({planId},{empID},{itemName},{itemdesp},400.96,150,100,150)""")
+      print("Successfully inserted data into customization and item tables")
+      return True
+  
+#Display PlanId
+def DisplayPlanId(custId):
+   if custId==""
+      return False
+   else:
+      cur.execute(f"""select Plan_ID from Customization_Detail where Cust_ID={custId}""")
+      return cur[0]
+
+#Store Questions
+def StoreQuestions():
+   
