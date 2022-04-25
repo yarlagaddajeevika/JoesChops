7d8f789ff0dcb13d77afc3b2823d6451b43f796b
chaowu214 <70608906+chaowu214@users.noreply.github.com>
2022-04-24 17:12:03 -0700

    Update the newest version

--- db.py
+++ db.py
@@ -2,252 +2,320 @@
 """ All functions will be called and run the query"""
 
 # pyodbc is the module to connect to sql server
-import pyodbc as connector
-#connection string with needed information
+# connection string with needed information
 """ The server name will be differ for all partners"""
-connection_string=(r"Driver={SQL Server};"
-               r"Server=LTCSUF-24KH0B3\SQLEXPRESS;" # please change this to your server--> run 'select @@SERVERNAME' in sql studio to find your server
-               r"Database=JOESCHOPS;"
-               r"Trusted_Connection=yes;")
+import pyodbc as connector
+connection_string = (r"Driver={ODBC Driver 17 for SQL Server};"
+                     r"Server=localhost;"
+                     r"PORT=1443;"
+                     r"Database=JOESCHOPS;"
+                     r"UID=sa;"
+                     r"PWD=Strong.Pwd-123;")
 
-#connect to the server and database under that server
-conn= connector.connect(connection_string)
+# connect to the server and database under that server
+conn = connector.connect(connection_string)
 
-#create a cursor to work on the database
-cur=conn.cursor()
+# create a cursor to work on the database
+cur = conn.cursor()
 
-##Login
+# Login
 # To authenticate user credentials - read from employee, customer data
-def authenticateUser(username,password,flag):
-   if username=="" or password=="":
-      return False
-   else:
-      cur.execute(f"""SELECT Customer.Cust_ID FROM Customer WHERE username='{username}' AND upassword='{password}'""")
-      customer=[]
-      if flag==True:
-         for i in cur:
+
+
+def authenticateCus(username, password):
+    if username == "" or password == "":
+        return None
+    else:
+        cur.execute(
+            f"""SELECT Customer.Cust_ID FROM Customer WHERE username='{username}' AND upassword='{password}'""")
+        customer = []
+        for i in cur:
             customer.append(i)
             if not customer:
-               return False
+                return None
             else:
-               return customer[0][0] # customer ID
-      else:
-         cur.execute(f"""SELECT Employee.Emp_ID FROM Employee WHERE username='{username}' AND upassword='{password}'""")
-         employee = []
-         for i in cur:
+                return customer[0][0]  # customer ID
+
+
+def authenticateEmp(username, password):
+    if username == "" or password == "":
+        return None
+    else:
+        cur.execute(
+            f"""SELECT Employee.Emp_ID FROM Employee WHERE username='{username}' AND upassword='{password}'""")
+        employee = []
+        for i in cur:
             employee.append(i)
             if not employee:
-               return False
+                return None
             else:
-               return employee[0][0]
+                return employee[0][0]
 
 # Insert entry into Customer table
-def createCustomer(fName, lName, mobile, email,uname, upswd, address, city, state, zip):
-   if uname=="" or upswd=="" or fName=="" or lName=="" or mobile=="" or email=="" or address=="" or city=="" or zip=="":
-      return False
-   else:
-      cur.execute(f"""INSERT into Customer values ('{fName}','{lName}',{mobile},'{email}','{uname}','{upswd}','{address}','{city}','{state}',{zip})""")    
-      cur.commit()
-      print("Successfully inserted row into customer table")
-      return True
 
-##Customer
-#Insert data into vehicle table
+
+def createCustomer(fName, lName, mobile, email, uname, upswd, address, city, state, zip):
+    if uname == "" or upswd == "" or fName == "" or lName == "" or mobile == "" or email == "" or address == "" or city == "" or zip == "":
+        return False
+    else:
+        cur.execute(
+            f"""INSERT into Customer values ('{fName}','{lName}',{mobile},'{email}','{uname}','{upswd}','{address}','{city}','{state}',{zip})""")
+        cur.commit()
+        print("Successfully inserted row into customer table")
+        return True
+
+# Customer
+# Insert data into vehicle table
+
+
 def StoreVehicleInfo(custId, producedBy, ModelNo, year, engineDet, Tirm, exterior, interior, bodyC, frameC, interC, engC):
-   if custId=="" or year=="":
-      return False
-   else:
-      cur.execute(f"""INSERT into Vehicle values ({custId},'{producedBy}','{ModelNo}',{year},'{engineDet}','{Tirm}','{exterior}','{interior}','{bodyC}','{frameC}','{interC}','{engC}')""")
-      cur.commit()
-      print("Successfully inserted data into vehicle table")
-      return True
+    if custId == "" or year == "":
+        return False
+    else:
+        cur.execute(
+            f"""INSERT into Vehicle values ({custId},'{producedBy}','{ModelNo}',{year},'{engineDet}','{Tirm}','{exterior}','{interior}','{bodyC}','{frameC}','{interC}','{engC}')""")
+        cur.commit()
+        print("Successfully inserted data into vehicle table")
+        return True
 
-#Dropdown values to show available employees
+# Dropdown values to show available employees
+
+
 def lisOfAllEmployees():
-   cur.execute(f"""select Employee.Emp_firstName+Employee.Emp_lastName from Employee""")
-   employeeData=[]
-   for i in cur:
-      employeeData.append(i)
-   
-   print(employeeData[0][0])
-   return employeeData[0] # array with all employee names
+    cur.execute(
+        f"""select Employee.Emp_firstName+Employee.Emp_lastName from Employee""")
+    employeeData = []
+    for i in cur:
+        employeeData.append(i)
 
-#Store details about Customisation
+    print(employeeData[0][0])
+    return employeeData[0]  # array with all employee names
+
+# Store details about Customisation
+
+
 def StoreCustomisationDetails(custId, empName, itemName, itemdesp):
-   if custId=="":
-      return False
-   else:
-      cur.execute(f"""select Employee.Emp_ID from Employee where Employee.Emp_firstName+Employee.Emp_lastName='{empName}'""")
-      empID=[]
-      for i in cur:
-         empID.append(i)
-      # print(empID[0])
-      
-      cur.execute(f"""select Vehicle.VerificationIN from Vehicle where Vehicle.Cust_ID={custId}""")
-      vIn=[]
-      for i in cur:
-         vIn.append(i)
-      # print(vIn[0])
-      # print(custId)
+    if custId == "":
+        return False
+    else:
+        cur.execute(
+            f"""select Employee.Emp_ID from Employee where Employee.Emp_firstName+Employee.Emp_lastName='{empName}'""")
+        empID = []
+        for i in cur:
+            empID.append(i)
+        # print(empID[0])
 
-      #store data in Customization_Detail
-      cur.execute(f"""insert into Customization_Detail values({custId},{vIn[0][0]},{empID[0][0]})""")
-      cur.commit()
-      cur.execute(f"""select Plan_ID from Customization_Detail where Cust_ID={custId}""")
-      planId=[]
-      for i in cur:
-         planId.append(i)
+        cur.execute(
+            f"""select Vehicle.VerificationIN from Vehicle where Vehicle.Cust_ID={custId}""")
+        vIn = []
+        for i in cur:
+            vIn.append(i)
+        # print(vIn[0])
+        # print(custId)
 
-      cur.execute(f"""insert into Item values({planId[0][0]},{empID[0][0]},'{itemName}','{itemdesp}',400.96,150,100,150)""")
-      cur.commit()
-      print("Successfully inserted data into customization and item tables")
-      return True
-  
-#Display PlanId
+        # store data in Customization_Detail
+        cur.execute(
+            f"""insert into Customization_Detail values({custId},{vIn[0][0]},{empID[0][0]})""")
+        cur.commit()
+        cur.execute(
+            f"""select Plan_ID from Customization_Detail where Cust_ID={custId}""")
+        planId = []
+        for i in cur:
+            planId.append(i)
+
+        cur.execute(
+            f"""insert into Item values({planId[0][0]},{empID[0][0]},'{itemName}','{itemdesp}',400.96,150,100,150)""")
+        cur.commit()
+        print("Successfully inserted data into customization and item tables")
+        return True
+
+# Display PlanId
+
+
 def DisplayPlanId(custId):
-   if custId=="":
-      return False
-   else:
-      cur.execute(f"""select Plan_ID from Customization_Detail where Cust_ID={custId}""")
-      temp=[]
-      for i in cur:
-         temp.append(i)
-      return temp[0][0]
+    if custId == "":
+        return False
+    else:
+        cur.execute(
+            f"""select Plan_ID from Customization_Detail where Cust_ID={custId}""")
+        temp = []
+        for i in cur:
+            temp.append(i)
+        return temp[0][0]
 
-#Store Questions
-def StoreQuestions(custId, date, question): #date should be a string
-   if custId=="":
-      return False
-   else:
-      planId=DisplayPlanId(custId)
-      cur.execute(f"""insert into Questionnaire values({planId},'{date}','{question}',null)""")
-      cur.commit()
-      print("Successfully inserted data into Questionarrie")
-      return True
+# Store Questions
 
-#Store the initial deposit
-def initialPayment(custId, depositAmt, date):
-   if custId=="":
-      return False
-   else:
-     planId=DisplayPlanId(custId)
-     cur.execute(f"""insert into Customization_Plan values({planId},600.29,{depositAmt},'{date}','2022-08-02',440.16,20.16,600.29-{depositAmt},'Card','Created','{date}',null)""")
-     cur.commit()
-     print("inserted data into plan") 
-     return True
 
-#Return total price
+def StoreQuestions(custId, date, question):  # date should be a string
+    if custId == "":
+        return False
+    else:
+        planId = DisplayPlanId(custId)
+        cur.execute(
+            f"""insert into Questionnaire values({planId},'{date}','{question}',null)""")
+        cur.commit()
+        print("Successfully inserted data into Questionarrie")
+        return True
+
+# Store the initial deposit
+
+
+# def initialPayment(custId, depositAmt, date):
+#     if custId == "":
+#         return False
+#     else:
+#         planId = DisplayPlanId(custId)
+#         cur.execute(
+#             f"""insert into Customization_Plan values({planId},600.29,{depositAmt},'{date}','2022-08-02',440.16,20.16,600.29-{depositAmt},'Card','Created','{date}',null)""")
+#         cur.commit()
+#         print("inserted data into plan")
+#         return True
+
+# Return total price
+
+
 def returnTotalPrice(custId):
-   if custId=="":
-      return False
-   else:
-      planId=DisplayPlanId(custId)
-      cur.execute(f"""select TE_price from Customization_Plan where Plan_ID={planId}""")
-      price=[]
-      for i in cur:
-         price.append(i)
+    if custId == "":
+        return False
+    else:
+        planId = DisplayPlanId(custId)
+        cur.execute(
+            f"""select TE_price from Customization_Plan where Plan_ID={planId}""")
+        price = []
+        for i in cur:
+            price.append(i)
 
-      return price[0][0]
+        return price[0][0]
 
-#Display customizations for employee
+# Display customizations for employee
+
+
 def getEmployeeCustomisations(empId):
-   if empId=="":
-      return False
-   else:
-      cur.execute(f"""select Plan_ID from Customization_Detail where Emp_ID={empId}""" )
-      empPland = []
-      for i in cur:
-         empPland.append(i)
-      cur.execute(f"""select Item.Plan_ID,Item.Item_ID,Item.Item_name,Item.Item_Desp,Customization_Plan.TE_price,Customization_Plan.Amount_Deposited,Customization_Plan.Estimated_DeliveryDate,Customization_Plan.Amount_due,
-         Customization_Plan.Status from Customization_Plan,Item where Customization_Plan.Plan_ID={empPland[0][0]} and Item.Emp_ID={empId}""" )
-      result=[]
-      for i in cur:
-         result.append(i)
-      return result[0] # returns the complete data in array format might be in ()
+    if empId == "":
+        return False
+    else:
+        cur.execute(
+            f"""select Plan_ID from Customization_Detail where Emp_ID={empId}""")
+        empPland = []
+        for i in cur:
+            empPland.append(i)
+        cur.execute(f"""select Item.Plan_ID,Item.Item_ID,Item.Item_name,Item.Item_Desp,Customization_Plan.TE_price,Customization_Plan.Amount_Deposited,Customization_Plan.Estimated_DeliveryDate,Customization_Plan.Amount_due,
+         Customization_Plan.Status from Customization_Plan,Item where Customization_Plan.Plan_ID={empPland[0][0]} and Item.Emp_ID={empId}""")
+        result = []
+        for i in cur:
+            result.append(i)
+        # returns the complete data in array format might be in ()
+        return result[0]
 
-#Update photo link, delivery date
+# Update photo link, delivery date
+
+
 def UpdateDB(empId, photoLink, DeliveryDate):
-   cur.execute(f"""select Plan_ID from Customization_Detail where Emp_ID={empId}""" )
-   empPland = []
-   for i in cur:
-      empPland.append(i)
-   if photoLink=="" and DeliveryDate!="":
-      cur.execute(f"""update Customization_Plan set Estimated_DeliveryDate='{DeliveryDate}' where Plan_ID={empPland[0][0]}""" )
-      cur.commit()
-      print("Successfully updated delivery date")
-   elif photoLink!="" and DeliveryDate=="":
-      cur.execute(f"""update Customization_Plan set Photo_links='{photoLink}' where Plan_ID={empPland[0][0]}""" )
-      cur.commit()
-      print("Successfully updated photo link") 
-   elif photoLink!="" and DeliveryDate!="":
-      cur.execute(f"""update Customization_Plan set Estimated_DeliveryDate='{DeliveryDate}',Photo_links='{photoLink}' where Plan_ID={empPland[0][0]}""" )
-      cur.commit()
-      print("Successfully updated both") 
-   
-   return True
+    cur.execute(
+        f"""select Plan_ID from Customization_Detail where Emp_ID={empId}""")
+    empPland = []
+    for i in cur:
+        empPland.append(i)
+    if photoLink == "" and DeliveryDate != "":
+        cur.execute(
+            f"""update Customization_Plan set Estimated_DeliveryDate='{DeliveryDate}' where Plan_ID={empPland[0][0]}""")
+        cur.commit()
+        print("Successfully updated delivery date")
+    elif photoLink != "" and DeliveryDate == "":
+        cur.execute(
+            f"""update Customization_Plan set Photo_links='{photoLink}' where Plan_ID={empPland[0][0]}""")
+        cur.commit()
+        print("Successfully updated photo link")
+    elif photoLink != "" and DeliveryDate != "":
+        cur.execute(
+            f"""update Customization_Plan set Estimated_DeliveryDate='{DeliveryDate}',Photo_links='{photoLink}' where Plan_ID={empPland[0][0]}""")
+        cur.commit()
+        print("Successfully updated both")
 
-#display questions to employee
+    return True
+
+# display questions to employee
+
+
 def Questions(empId):
-   cur.execute(f"""select Plan_ID from Customization_Detail where Emp_ID={empId}""" )
-   empPland = []
-   for i in cur:
-      empPland.append(i)
+    cur.execute(
+        f"""select Plan_ID from Customization_Detail where Emp_ID={empId}""")
+    empPland = []
+    for i in cur:
+        empPland.append(i)
 
-   cur.execute(f"""select Question_No,Question_Desp from Questionnaire where Plan_ID={empPland[0][0]}""")
-   result=[]
-   for i in cur:
-      result.append(i)
-   return result[0] # returns an array of questions
+    cur.execute(
+        f"""select Question_No,Question_Desp from Questionnaire where Plan_ID={empPland[0][0]}""")
+    result = []
+    for i in cur:
+        result.append(i)
+    return result[0]  # returns an array of questions
 
-#Update question answers
-def UpdateQuestionsAns(empId,quesNo,quesAns):
-   cur.execute(f"""select Plan_ID from Customization_Detail where Emp_ID={empId}""" )
-   empPland = []
-   for i in cur:
-      empPland.append(i)
-   
-   cur.execute(f"""update Questionnaire set Question_Ans='{quesAns}' where Question_No='{quesNo}' and Plan_ID={empPland[0][0]}""")
-   cur.commit()
-   print("Updated question answers")
-      
-#display answers to customers
+# Update question answers
+
+
+def UpdateQuestionsAns(empId, quesNo, quesAns):
+    cur.execute(
+        f"""select Plan_ID from Customization_Detail where Emp_ID={empId}""")
+    empPland = []
+    for i in cur:
+        empPland.append(i)
+
+    cur.execute(
+        f"""update Questionnaire set Question_Ans='{quesAns}' where Question_No='{quesNo}' and Plan_ID={empPland[0][0]}""")
+    cur.commit()
+    print("Updated question answers")
+
+# display answers to customers
+
+
 def DisplayAnswers(custId):
-   planId = DisplayPlanId(custId)
-   cur.execute(f"""select Question_No, Question_Desp, Question_Ans from Questionnaire where Plan_ID={planId}""")
-   ans=[]
-   for i in cur:
-      ans.append(i)
-   
-   return ans # array
+    planId = DisplayPlanId(custId)
+    cur.execute(
+        f"""select Question_No, Question_Desp, Question_Ans from Questionnaire where Plan_ID={planId}""")
+    ans = []
+    for i in cur:
+        ans.append(i)
 
-#delete vehicle details
+    return ans  # array
+
+# delete vehicle details
+
+
 def DeleteVehicle(empId):
-   cur.execute(f"""select Plan_ID from Customization_Detail where Emp_ID={empId}""" )
-   empPland = []
-   for i in cur:
-      empPland.append(i)
+    cur.execute(
+        f"""select Plan_ID from Customization_Detail where Emp_ID={empId}""")
+    empPland = []
+    for i in cur:
+        empPland.append(i)
 
-   cur.execute(f"""delete from Customization_Plan where Plan_ID={empPland[0][0]}""")
-   cur.execute(f"""delete from Questionnaire where Plan_ID={empPland[0][0]}""")
-   cur.execute(f"""delete from Item where Plan_ID={empPland[0][0]}""")
+    cur.execute(
+        f"""delete from Customization_Plan where Plan_ID={empPland[0][0]}""")
+    cur.execute(
+        f"""delete from Questionnaire where Plan_ID={empPland[0][0]}""")
+    cur.execute(f"""delete from Item where Plan_ID={empPland[0][0]}""")
 
-   cur.execute(f"""select VerificationIN from Customization_Detail where Emp_ID={empId}""")
-   vIn=[]
-   for i in cur:
-      vIn.append(i)
+    cur.execute(
+        f"""select VerificationIN from Customization_Detail where Emp_ID={empId}""")
+    vIn = []
+    for i in cur:
+        vIn.append(i)
 
-   cur.execute(f"""delete from Customization_Detail where VerificationIN={vIn[0][0]}""")
-   cur.execute(f"""delete from Vehicle where VerificationIN={vIn[0][0]}""")
-   print("Deleted vehicle details")
+    cur.execute(
+        f"""delete from Customization_Detail where VerificationIN={vIn[0][0]}""")
+    cur.execute(f"""delete from Vehicle where VerificationIN={vIn[0][0]}""")
+    print("Deleted vehicle details")
 
-#Search with PlanID - customer
+# Search with PlanID - customer
+
+
 def searchData(planId):
-   cur.execute(f"""select Status,Estimated_DeliveryDate,Amount_due,Photo_links,Item.Item_name,Item.Item_Desp from Customization_Plan 
+    cur.execute(f"""select Status,Estimated_DeliveryDate,Amount_due,Photo_links,Item.Item_name,Item.Item_Desp from Customization_Plan 
      inner JOIN Item on Customization_Plan.Plan_ID={planId} and Item.Plan_ID={planId}""")
-   
-   searchData=[]
 
-   for i in cur:
-      searchData.append(i)
+    searchData = []
 
-   return searchData # returns array
+    for i in cur:
+        searchData.append(i)
+
+    return searchData  # returns array
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from distutils.log import error
 from email import message
 from http.client import FOUND
 import db
@@ -15,13 +16,13 @@ def index():
     return render_template('home.html')
 
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
+@app.route('/cuslogin', methods=['POST', 'GET'])
+def cuslogin():
     if request.method == "POST":
         session.pop('username', None)
         username = request.form.get('username', None)
         password = request.form.get('password', None)
-        rt = db.authenticateUser(username, password)
+        rt = db.authenticateCus(username, password)
         if rt is None:
             return render_template('home.html', message="Login failed.")
 
@@ -32,7 +33,24 @@ def login():
 
     else:
         return render_template('home.html', message="Login failed.")
-    return render_template('home.html')
+    return render_template('home.html', message=customer_id)
+
+
+@app.route('/emplogin', methods=['POST', 'GET'])
+def emplogin():
+    if request.method == "POST":
+        session.pop('username', None)
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+        rt = db.authenticateEmp(username, password)
+        if rt is None:
+            return render_template('home.html', message="Login failed.")
+
+        print(username)
+        print(password)
+    else:
+        return render_template('home.html', message="Login failed.")
+    return render_template('home.html', message="Login Successfuly.")
 
 
 @app.route('/register', methods=['POST', 'GET'])
@@ -50,14 +68,14 @@ def register():
         password = request.form.get('new_password', None)
         db.createCustomer(firstname, lastname, phone, email,
                           username, password, address, city, state, zip)
-    return render_template('home.html', message="Register Successfully.")
+        return render_template('home.html', message="Register Successfully.")
+    return render_template('home.html', message="Register Fail.")
 
 
 @app.route('/vehicleInput', methods=['POST', 'GET'])
 def vehicleInput():
     if request.method == "POST":
         id = request.form.get('new_id', None)
-        # plate = request.form.get('new_plate', None)
         brand = request.form.get('new_brand', None)
         model = request.form.get('new_model', None)
         year = request.form.get('new_year', None)
@@ -71,7 +89,8 @@ def vehicleInput():
         engineCon = request.form.get('new_engineCon', None)
         db.StoreVehicleInfo(id, brand, model, year, engine, trim,
                             interior, exterior, condition, frame, interiorCon, engineCon)
-    return render_template('vehicle.html', message="Register Successfully.")
+        return render_template('vehicle.html', message="Register Successfully.")
+    return render_template('vehicle.html', message="Fail.")
 
 
 @app.route('/searchEmp')
@@ -93,7 +112,8 @@ def customize():
         print(itemName)
         print(itemDescri)
         db.StoreCustomisationDetails(id, empName, itemName, itemDescri)
-    return render_template('customization.html', message="Submit Successfully.")
+        return render_template('customization.html', message="Submit Successfully.")
+    return render_template('customization.html', message="Fail.")
 
 
 @app.route('/getPlanid', methods=['POST', 'GET'])
@@ -101,7 +121,8 @@ def getPlanid():
     if request.method == "POST":
         planID = request.form.get('planID', None)
         str = db.searchData(planID)
-    return render_template('cusReport.html', message=str)
+        return render_template('cusReport.html', message=str)
+    return render_template('cusReport.html', message="Fail.")
 
 
 @app.route('/inputQuestions', methods=['POST', 'GET'])
@@ -115,7 +136,8 @@ def inputQuestions():
         print(questions)
 
         db.StoreQuestions(cusID, date, questions)
-    return render_template('cusReport.html', message="Submit Successfully.")
+        return render_template('cusReport.html', message="Submit Successfully.")
+    return render_template('cusReport.html', message="Fail.")
 
 
 @app.route('/getTotal', methods=['POST', 'GET'])
@@ -135,10 +157,57 @@ def getEmpid():
     if request.method == "POST":
         empID = request.form.get('empID', None)
         result = db.getEmployeeCustomisations(empID)
-        result_list = []
-        for r in result:
-            result_list.append(r)
-    return render_template('showPlan.html', message=result_list[0])
+        return render_template('showPlan.html', message=result)
+    return render_template('showPlan.html', message="Fail.")
+
+
+@app.route('/update', methods=['POST', 'GET'])
+def updateDB():
+    if request.method == "POST":
+        empID = request.form.get('empID', None)
+        link = request.form.get('link', None)
+        date = request.form.get('date', None)
+        db.UpdateDB(empID, link, date)
+        return render_template('updateProgress.html', message="Update Successfully")
+    return render_template('updateProgress.html', message="Fail.")
+
+
+@app.route('/getQuestion', methods=['POST', 'GET'])
+def getQuestion():
+    if request.method == "POST":
+        empID = request.form.get('empID', None)
+        questions = db.Questions(empID)
+        return render_template('ansQuestions.html', message=questions)
+    return render_template('ansQuestions.html', message="Fail.")
+
+
+@app.route('/ansQuestion', methods=['POST', 'GET'])
+def ansQuestion():
+    if request.method == "POST":
+        empID = request.form.get('empID', None)
+        quesNo = request.form.get('quesNo', None)
+        quesAns = request.form.get('quesAns', None)
+        db.UpdateQuestionsAns(empID, quesNo, quesAns)
+        return render_template('ansQuestions.html', message="Update Successfully")
+    return render_template('ansQuestions.html', message="Fail.")
+
+
+@app.route('/getAns', methods=['POST', 'GET'])
+def getAns():
+    if request.method == "POST":
+        custID = request.form.get('custID', None)
+        ans = db.DisplayAnswers(custID)
+        return render_template('cusReport.html', message=ans)
+    return render_template('cusReport.html', message="Fail.")
+
+
+@app.route('/deleteV', methods=['POST', 'GET'])
+def deleteV():
+    if request.method == "POST":
+        empID = request.form.get('empID', None)
+        db.DeleteVehicle(empID)
+        return render_template('delete.html', message="Delete Successfully")
+    return render_template('delete.html', message="Fail.")
 
 
 @app.route('/customer')
@@ -189,6 +258,11 @@ def updateProgress():
 @app.route('/ansQuestions')
 def ansQuestions():
     return render_template('ansQuestions.html')
+
+
+@app.route('/delete')
+def delete():
+    return render_template('delete.html')
 
 
 @app.route('/about')
